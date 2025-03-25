9061665fd26fb704d1e58b1a682d4b005b613388
TiaWidi <tia.septianawidi@gmail.com>
2025-03-25 22:59:06 +0700

    Try to enhance layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Basic gate definition to check user role
+        Gate::define('manage-blog', function (User $user) {
+            // return $user->role === 'admin';
+            return true;
+        });
     }
 
     /**
